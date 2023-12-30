7ad1cbb8885a47793f031483e91a1c92c8fdf6b1
Gabriel Melo <meloferreiragabriel17@gmail.com>
2023-12-30 13:50:20 -0300

    Commit Inicial

---
+++ src/main/java/com/picpay/app/PicPayApplication.java
@@ -0,0 +1,13 @@
+package com.picpay.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicPayApplication.class, args);
+	}
+
+}
